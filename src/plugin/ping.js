7a1b34362ec05d96f626cca71b78618668dbf57b
Ednut001 <Kushimotunde7@gmail.com>
2024-07-11 19:05:02 +0100

    Ednut

---
+++ src/plugin/ping.js
@@ -0,0 +1,16 @@
+const ping = async (m, sock) => {
+  const prefix = /^[\\/!#.]/gi.test(m.body) ? m.body.match(/^[\\/!#.]/gi)[0] : '/';
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).toLowerCase() : '';
+
+  if (cmd === "ping") {
+    const start = new Date().getTime();
+    await m.React('⚡');
+    const end = new Date().getTime();
+    const responseTime = (end - start) / 1000;
+
+    const text = `*Eᴅɴᴜᴛ Mᴅ_🌩️⃝вσт  : ${responseTime.toFixed(2)} sᴘᴇᴇᴅ_*`;
+    sock.sendMessage(m.from, { text }, { quoted: m });
+  }
+}
+
+export default ping;
